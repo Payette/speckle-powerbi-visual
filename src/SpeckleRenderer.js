a69fa0eb0f933d3f5ac0f1cb714a7b675e5b2066
Gil Rosenthal <grosenthal@payette.com>
2020-06-25 16:39:35 -0400

    Better fix

--- src/SpeckleRenderer.js
+++ src/SpeckleRenderer.js
@@ -423,8 +423,8 @@ export default class SpeckleRenderer {
         // console.log("Zooming to filtered")
         this.computeSceneBoundingSphere()
         this.resetCamera(true)
-        // this.zoomExtents()
-        if(this.selectedObjects.length > 0) this.zoomToObject(this.selectedObjects[0])
+        this.zoomHighlightExtents()
+        // if(this.selectedObjects.length > 0) this.zoomToObject(this.selectedObjects[0])
       }
 
     })
